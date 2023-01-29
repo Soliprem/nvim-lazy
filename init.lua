4c88ad604717f9d0574bf73efa87d4f54a5c4710
soliprem <franci.solidoro@gmail.com>
2023-01-29 03:18:06 +0100

    new

---
+++ init.lua
@@ -0,0 +1,15 @@
+require("lazyvim")
+require("lazy").setup("plugins")
+require("config").setup()
+for _, source in ipairs({
+	"util",
+	"config",
+	"config.options",
+	"config.autocmds",
+	"config.keymaps",
+}) do
+	local status_ok, fault = pcall(require, source)
+	if not status_ok then
+		vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+	end
+end
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+	},
+	-- tokyonight
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = { style = "moon" },
+	},
+
+	-- catppuccin
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+	},
+}
